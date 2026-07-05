c3bc5f565e01a110f02436cbbcb6bfaaa0784763
Eloy García Almadén <eloy.garcia.pca@gmail.com>
2026-07-05 19:31:32 +0200

    Fixing some problems related to create de image for production

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,12 @@
 import NextAuth, { NextAuthOptions } from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 
+declare module "next-auth" {
+  interface Session {
+    accessToken?: string;
+  }
+}
+
 export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
